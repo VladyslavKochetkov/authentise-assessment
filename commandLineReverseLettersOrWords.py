e815b7cf396ed28492e203a1d2ad783336dca5af
Vladyslav Kochetkov <me@vladk.dev>
2022-01-13 12:54:22 -0500

    pretty print with text in commandLineReverseLettersOrWords

--- commandLineReverseLettersOrWords.py
+++ commandLineReverseLettersOrWords.py
@@ -9,4 +9,8 @@ helpString = """Usage: python3 commandLineReverseLettersOrWords.py 'String to mo
 
 flag, stringOrFile = itemgetter("flag", "stringOrFile")(getCommandLineInputs(helpString))
 
-print(reverseString(stringOrFile) if flag == "-r" else reverseOrderOfWordsInString(stringOrFile))
+print('Reversed the {reverseString} from "{original}" to "{reversed}"'.format(
+    reverseString = "order of the string" if flag == "-r" else "order of the words in the string",
+    original = stringOrFile,
+    reversed = reverseString(stringOrFile) if flag == "-r" else reverseOrderOfWordsInString(stringOrFile) 
+))
